d95708df67aa10434238026f9ec6729c66e59e09
crmendoza <c.roanne.mendoza@gmail.com>
2019-03-19 06:53:32 +0900

    Implementation of Atbash Cipher

---
+++ Ciphers.playground/Contents.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+func atbashCipher(_ plainText: String) -> String {
+    let alphabet = Array("abcdefghijklmnopqrstuvwxyz")
+    let cipher = zip(alphabet, alphabet.reversed()).reduce([String: String]()) { (dict, arg1) -> [String: String] in
+        let (key, value) = arg1
+        var dict = dict
+        dict[String(key)] = String(value)
+        return dict
+    }
+    
+    let cipherText = Array(plainText.lowercased().replacingOccurrences(of: " ", with: "")).compactMap { String($0) }.map { (str) -> String in
+        return cipher[str] ?? ""
+    }
+    return cipherText.joined()
+}
+
+func atbashCipher2(_ plainText: String) -> String {
+    let alphabet = Array("abcdefghijklmnopqrstuvwxyz")
+    let cipherText =  Array(plainText.lowercased().replacingOccurrences(of: " ", with: "")).compactMap { (char) -> String? in
+        if let index = alphabet.index(of: char) {
+            return String(alphabet[alphabet.count - 1 - index])
+        }
+        return nil
+    }
+    
+    return cipherText.joined()
+}
+
+atbashCipher("HelloWorld")
+atbashCipher2("HelloWorld")
